e21552de0b81fe1b5ce6ebd92e469e278832488e
gdmec07150847 <1103249267@qq.com>
2016-12-20 21:04:27 +0800

    ziyang

---
+++ app/src/main/java/cn/edu/gdmec/chaos07150844/myguard/m2theftguard/BaseSetUpActivity.java
@@ -0,0 +1,18 @@
+package cn.edu.gdmec.chaos07150844.myguard.m2theftguard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.edu.gdmec.chaos07150844.myguard.R;
+
+
+public class BaseSetUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_set_up);
+
+
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/chaos07150844/myguard/m2theftguard/entity/ContactInfo.java
@@ -0,0 +1,10 @@
+package cn.edu.gdmec.chaos07150844.myguard.m2theftguard.entity;
+
+/**
+ * Created by asus on 2016/12/20.
+ */
+public class ContactInfo {
+    public String id;
+    public String name;
+    public String phone;
+}
