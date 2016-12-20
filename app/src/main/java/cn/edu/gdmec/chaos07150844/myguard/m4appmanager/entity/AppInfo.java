8b6dbea31eb767a67197bb8fc38d249006ac6e8c
gdmec07150844 <daaixiaoliu@qq.com>
2016-12-20 20:27:26 +0800

    66 new

--- app/src/main/java/cn/edu/gdmec/chaos07150844/myguard/m4appmanager/entity/AppInfo.java
+++ app/src/main/java/cn/edu/gdmec/chaos07150844/myguard/m4appmanager/entity/AppInfo.java
@@ -11,11 +11,11 @@ public class AppInfo {
     public String appName;
     public String apkPath;
     public long appSize;
-    public boolean isInRom;
+    public boolean isInRoom;
     public boolean isUserApp;
     public boolean isSelected = false;
-    public String getAppLocation(boolean isInRom){
-        if (isInRom){
+    public String getAppLocation(boolean isInRoom){
+        if (isInRoom){
             return "手机内存";
         }else{
             return "外部存储";
