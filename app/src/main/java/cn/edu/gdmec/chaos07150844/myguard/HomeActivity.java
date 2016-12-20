921e7452333b4bd0c1a3f8d33802dbec78444022
gdmec07150844 <daaixiaoliu@qq.com>
2016-12-20 14:14:55 +0800

    66 new

---
+++ app/src/main/java/cn/edu/gdmec/chaos07150844/myguard/HomeActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.chaos07150844.myguard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
