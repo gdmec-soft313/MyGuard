ffd27455f2b777799da9d461be43bbaf4c3d156b
gdmec07150848 <1198381338@qq.com>
2016-12-22 14:34:42 +0800

    mian

---
+++ app/src/main/java/cn/edu/gdmec/chaos07150844/myguard/m5virusscan/entity/ScanAppInfo.java
@@ -0,0 +1,14 @@
+package cn.edu.gdmec.chaos07150844.myguard.m5virusscan.entity;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by flora on 2016/12/21.
+ */
+public class ScanAppInfo {
+    public String appName;
+    public boolean isVirus;
+    public String packagename;
+    public String description;
+    public Drawable appicon;
+}
